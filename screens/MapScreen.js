284f0731e67972ab5dbab1ab26b5c2cce33d233d
Elyelin Carrasquero <elyelin15@gmail.com>
2022-06-11 12:27:28 -0300

    map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,22 +1,52 @@
-import React from 'react'
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, {useEffect} from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import MapView, { Marker, Pilyline } from "react-native-maps";
+import { getCurrentLocation } from '../utils/helpers';
 
-const MapScreen = ({navigation}) => {
+const MapScreen = ({ navigation }) => {
+
+  const orientacion = {
+    long: -58.528763,
+    latit: -34.558106
+    // long: -84.419853,
+    // latit: 33.640411
+  };
+
+  React.useEffect(() => {
+    getCurrentLocation()
+  }, [])
+
+
   return (
-    <View>
-        <Text>Maps</Text>
-        <Button title="Click Here" onPress={() => alert("Button Clicked!")}/>
+    <View styles={styles.container}>
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        initialRegion={{
+          latitude: orientacion.latit,
+          longitude: orientacion.long,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.04,
+        }}
+        mapType='standard'
+      >
+
+        <Marker>
+        </Marker>
+      {/* <Marker key={1} coordinate={{latitude: orientacion.latit, longitude: orientacion.long}}/> */}
+        
+        </MapView>
+      {/* <Button title="Click Here" onPress={() => alert("Button Clicked!")}/> */}
     </View>
   );
-}
+};
 
 export default MapScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#8fcbbc"
-    },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#8fcbbc",
+  },
 });
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const ProfileScreen = ({navigation}) => {
   return (
-    <View>
+    <View style={styles.container}>
         <Text>Mi Perfil</Text>
     </View>
   );
